07643aed7a3f6755eb1e7adb451509e2174ab1e0
faali03 <113978218+faali03@users.noreply.github.com>
2022-10-20 09:26:48 +0200

    Add files via upload

---
+++ ej5.java
@@ -0,0 +1,11 @@
+public class ej5 {
+  public static void main(String[] args) {
+    int i = 320;
+    
+    while(i >= 160) {
+        System.out.println(i);
+        i-=20;
+    }
+  }
+}
+
---
+++ ej8.java
@@ -0,0 +1,12 @@
+public class ej8 {
+  public static void main(String[] args) {
+    
+    System.out.print("Introduzca un número y le mostraré su tabla de multiplicar: ");
+    int numeroIntroducido = Integer.parseInt(System.console().readLine());
+        
+    for (int i = 0; i <= 10; i++) {
+      System.out.printf("%d x %2d = %3d\n", numeroIntroducido, i, numeroIntroducido * i);
+    }
+
+  }
+}
